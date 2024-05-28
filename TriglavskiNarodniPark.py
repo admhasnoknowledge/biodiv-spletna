d62098d1a44db12708640227d4243d8ba42d17ec
adamhasnoknowledge <69860078+admhasnoknowledge@users.noreply.github.com>
2024-05-28 23:02:37 +0200

    d

--- TriglavskiNarodniPark.py
+++ TriglavskiNarodniPark.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="Triglavski Narodni Park", page_icon=":🍃:", lay
 
 st.title("Triglavski Narodni Park")
 st.markdown("### To je moja naloga za Biologijo v 9. razredu, če imaš kot bralec kakršenkoli predlog, komentar, mi ga pošlji na e-pošto. Če pa ne, pa upam da uživaš v branju! Za začetek ti priporočam uvod." )
-st.page_link("pages/2_⛰_uvod.py", label="uvod", icon="🧧")
+
 
 st.sidebar.success("Izberi stran, ki si jo želiš prebrati")
 
